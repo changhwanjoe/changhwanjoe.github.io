23d1a45d93833ef57b818f5836763afe2d04ab80
changhwan.joe <chjoe01@gmail.com>
2026-05-01 16:05:55 +0900

    feat:add modern Cv format and update

---
+++ cv/modern/data/cv-data.js
@@ -0,0 +1,67 @@
+export const cvData = {
+  profile: {
+    name: "Changhwan Joe",
+    role: "Python Developer",
+    location: "South Korea",
+    email: "chjoe01@gmail.com",
+    github: "https://github.com/changhwanjoe",
+    linkedin: "http://chaloalto.tistory.com"
+  },
+  summary:
+    "Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.",
+  skills: [
+    "Python & Django",
+    "Javascript & jQuery",
+    "Angular",
+    "HTML5 & CSS",
+    "Ruby on Rails",
+    "Sketch & Photoshop"
+  ],
+  experience: [
+    {
+      title: "Lead Developer",
+      organization: "Startup Hubs, San Francisco",
+      period: "2015 - Present",
+      details: [
+        "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.",
+        "Bullet point",
+        "Bullet point"
+      ]
+    },
+    {
+      title: "Senior Software Engineer",
+      organization: "Google, London",
+      period: "2014 - 2015",
+      details: [
+        "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+        "Bullet point",
+        "Bullet point"
+      ]
+    },
+    {
+      title: "UI Developer",
+      organization: "Amazon, London",
+      period: "2012 - 2014",
+      details: [
+        "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+        "Bullet point",
+        "Bullet point"
+      ]
+    }
+  ],
+  education: [
+    {
+      degree: "MSc in Computer Engineering",
+      school: "Korea University",
+      period: "2018 - 2021"
+    },
+    {
+      degree: "BSc in Electrical Engineering",
+      school: "Korea University",
+      period: "2012 - 2018"
+    }
+  ]
+};
